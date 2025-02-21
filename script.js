00c2fbf63a313aae6e8009d5171f17608991eb7c
harilal766 <harilasunil2@gmail.com>
2025-02-21 13:48:28 +0530

    calc structure added

---
+++ script.js
@@ -0,0 +1,23 @@
+// initialize the display and other divs
+
+const numButtons = document.getElementById("numButtons");
+
+// 3 x 3 grid for buttons 1 to 9
+const grid = 3;
+let digit = 0;
+for (let row=0; row<grid; row++){
+    // create button row
+    const buttonRow = document.createElement("div");
+    buttonRow.classList.add("row");
+    // create button for each row
+    for (let button =1; button<=grid; button++){
+        const numButton = document.createElement("button");
+        digit += 1;
+        numButton.innerText = digit;
+        numButton.id = digit;
+        numButton.classList.add("button");
+        buttonRow.appendChild(numButton);
+    }
+    numButtons.appendChild(buttonRow);
+}
+
