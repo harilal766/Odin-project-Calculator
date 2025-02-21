70ff96ec592a2abd10404990c7e48d5afa6d1e13
harilal766 <harilalsunil2@gmai.com>
2025-02-21 22:07:05 +0530

    added functions for each operation

--- script.js
+++ script.js
@@ -22,61 +22,18 @@ for (let row=0; row<grid; row++){
 
 
 
+function add(num,anotherNum){
+    return num + anotherNum;
+}
 
+function subtract(num,anotherNum){
+    return num - anotherNum;
+}
 
+function multiply(num,anotherNum){
+    return num * anotherNum;
+}
 
-
-
-
-
-// PROCESSING BUTTON CLICKS
-const questionText = document.getElementById("questionText");
-const resetButton = document.getElementById("reset");
-let verifiedText = ""; // question text need verificaions on each key press
-let buttons = document.querySelectorAll("button"); 
-
-
-let calcStatus = null; let statusDisplay = document.getElementById("status");
-
-// lhs, rhs and operator
-let num = 0; let anotherNum=0; let operator = null;
-// limit to the question text based on the display width
-let lengthLimit = 15;
-let dispInput = ''; let inputNum = '';
-
-let questionArray = [];
-
-let decimalCount =0; operatorCount = 0;
-
-
-let clickedButtonContent; 
-buttons.forEach((button) =>{
-    button.addEventListener("click",function(event){
-        clickedButtonContent = event.target.innerText;
-        // number verification
-        if (!isNaN(clickedButtonContent)){
-            // add digit by mulitplying with 10
-            inputNum += clickedButtonContent;
-            dispInput = `${inputNum}`;
-        }
-        else{// possibilites : 00/decimal/operator/reset/=
-            if (["+","-","*","/"].includes(clickedButtonContent)){
-                if (operatorCount === 0){
-                    dispInput += `${clickedButtonContent}`;
-                    operator = 1;
-                    if (questionArray.length < 3){
-                        questionArray.push(inputNum);
-                        questionArray.push(clickedButtonContent)
-                    }
-                    
-                }
-            }
-        }
-
-
-        questionText.textContent = dispInput;
-        console.log(clickedButtonContent,questionArray);
-    });
-
-});
-
+function divide(num,anotherNum){
+    return num/anotherNum;
+}
